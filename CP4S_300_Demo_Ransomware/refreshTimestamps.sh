fae107666848d1e5b8f4b1d87336b05bdf67144f
cp4s-content <89663068+cp4s-content@users.noreply.github.com>
2021-08-27 16:16:55 -0400

    Add files via upload

--- CP4S_300_Demo_Ransomware/refreshTimestamps.sh
+++ CP4S_300_Demo_Ransomware/refreshTimestamps.sh
@@ -3,8 +3,9 @@
 # requires the gsed (GNU sed) command to be installed on mac through homebrw
 # Jonathan Tomasulo - Cybersecurity Specialist
 
-#currentDate=`date '+%FT%T.000Z'`
-currentDate='2031-08-27T15:49:50.000Z'
+currentDate=`date '+%FT%T.000Z'`
+futureDate='"last_observed": "2031-08-27T15:49:50.000Z'
 echo "$currentDate"
 gsed -i "s/[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]T[0-9][0-9]:[0-9][0-9]:[0-9][0-9]\.[0-9][0-9][0-9]Z/$currentDate/g" *.json
+gsed -i "s/\"last_observed\": \"[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]T[0-9][0-9]:[0-9][0-9]:[0-9][0-9]\.[0-9][0-9][0-9]Z/$futureDate/g" *.json
 echo "Dates Updated to: $currentDate Successfully"
--- README.md
+++ README.md
@@ -3,4 +3,3 @@
 This repository contains all of the files that we use to demo cloud pak for security including all files for 21 demos.
 
 All files are accessible from:
-https://cp4s-content.github.io/21_demos/{{path_to_file}}
