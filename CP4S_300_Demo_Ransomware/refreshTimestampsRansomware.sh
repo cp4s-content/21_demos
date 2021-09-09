56687aa4bac8dd5cad35b59d087a21a626980711
Gerald Trotman <gerald.trotmanibm.com@MacBook-Pro-4.attlocal.net>
2021-09-09 10:32:16 -0700

    Made changes to the timestamp script to handle higher resolution of seconds for Ransomware STIX bundle. Added/renamed it to Ransomware directory to be used only for this use case. Ran script to successfully change last_observed date to future.

---
+++ CP4S_300_Demo_Ransomware/refreshTimestampsRansomware.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+# This file will update the date and time on the STIX bundles
+# requires the gsed (GNU sed) command to be installed on mac through homebrw
+# Jonathan Tomasulo - Cybersecurity Specialist
+
+currentDate=`date '+%FT%T.000000Z'`
+futureDate='"last_observed": "2031-08-27T15:49:50.000000Z'
+echo "$currentDate"
+gsed -i "s/[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]T[0-9][0-9]:[0-9][0-9]:[0-9][0-9]\.[0-9][0-9][0-9][0-9][0-9][0-9]Z/$currentDate/g" *.json
+gsed -i "s/\"last_observed\": \"[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]T[0-9][0-9]:[0-9][0-9]:[0-9][0-9]\.[0-9][0-9][0-9][0-9][0-9][0-9]Z/$futureDate/g" *.json
+echo "Dates Updated to: $futureDate Successfully"
